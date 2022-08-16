e471d231ca6dbc85666b8abee5485d1bece3594b
Sagor Mainul <mhs.branding@gmail.com>
2022-08-16 22:24:43 +0600

    NaN problem solved

--- js/withdraw.js
+++ js/withdraw.js
@@ -16,21 +16,31 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
   const withdrawField = document.getElementById('withdraw-field')
   const newWithdrawAmountString = withdrawField.value
   const newWithdrawAmount = parseFloat(newWithdrawAmountString)
+  //step-7:
+  withdrawField.value = ``
+  if (isNaN(newWithdrawAmount)) {
+    alert('Please enter a right amount')
+    return
+  }
   //step-3:
   const withdrawTotalElement = document.getElementById('withdraw-total')
   const previousWithdrawTotalString = withdrawTotalElement.innerText
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString)
   console.log(previousWithdrawTotal)
-  //step-4:
-  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount
-  withdrawTotalElement.innerText = currentWithdrawTotal
+
   //step-5:
   const balanceTotalElement = document.getElementById('balance-total')
   const previousBalanceTotalString = balanceTotalElement.innerText
   const previousBalanceTotal = parseFloat(previousBalanceTotalString)
+
+  if (newWithdrawAmount > previousBalanceTotal) {
+    alert('Bap er Bank e eto tk nai !!!!')
+    return
+  }
+  //step-4:
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount
+  withdrawTotalElement.innerText = currentWithdrawTotal
   //step-6:
   const newBalanceTotal = previousBalanceTotal - newWithdrawAmount
   balanceTotalElement.innerText = newBalanceTotal
-  //step-7:
-  withdrawField.value = ``
 })
